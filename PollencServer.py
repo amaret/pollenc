be5b19d60a7b013b0f4aa42051a97feb97597a96
megan adams <megan@amaret.com>
2014-06-18 13:15:41 -0700

    using sockets

---
+++ PollencServer.py
@@ -0,0 +1,218 @@
+#!/usr/bin/env python
+
+# Copyright 2012, 2013 Amaret Inc. All rights reserved.
+
+import sys
+import datetime
+import os
+
+#sys.path.append(sys.path[0] + os.sep + '..' + os.sep + 'wind.lib')
+#sys.path.insert(1, "/etc/amaret")
+#import config
+#import WindData
+
+import SocketServer
+import StringIO
+import threading
+import redis
+import syslog
+import json
+import traceback
+#from riemann import RiemannClient, RiemannUDPTransport
+
+#rmmonitor = RiemannClient(transport = RiemannUDPTransport, host=config.riemann['host'])
+
+REDIS_HOST = "redbis.wind.io"
+REDIS_PORT = 6379
+REDIS_QNAME = 'POLLEN_CLC_0_1'
+TCP_HOST= '0.0.0.0'
+TCP_PORT= 2323
+
+MAX_MSG_SIZE = 1000000
+
+ERROR_MSG_OBJ = {
+                'tid': 0, 
+                'aid': 0, 
+                'type': 'response', 
+                'service': 'compile', 
+                'user': {
+                    'id': 0, 
+                    'name': 
+                    'None'
+                    }, 
+                'project': {
+                    'id': 0, 
+                    'name': 
+                    'None'
+                    }, 
+                'content' : {
+                    'content': 'None', 
+                    'filename': 'None'
+                    }
+                }
+
+
+class PollencRequestHandler(SocketServer.BaseRequestHandler):
+
+    def getRdis(self):
+        return redis.Redis(REDIS_HOST, REDIS_PORT)
+
+    #
+    # begin redis usage
+    #
+    def getQName(self, compiler): 
+      return REDIS_QNAME
+
+    def write(self, qname, dstr):
+      self.getRdis().lpush(qname, dstr);
+    
+    def read(self, replyQueue):
+        response = self.getRdis().brpop(keys=[replyQueue], timeout=30);
+        if ( not response or not len(response) == 2) :
+            raise Exception('bad response from clc: %s' % (response))
+        return response[1]
+
+    def handleError(self, etxt):
+        syslog.syslog(syslog.LOG_ERR, etxt)
+        traceback.print_exc(file=sys.stdout)
+        self.sendErrorStats()     
+        ERROR_MSG_OBJ['content']['error'] = etxt
+        emsgtxt = json.dumps(ERROR_MSG_OBJ)
+        hmsg = "%i\n%s" % (len(emsgtxt), emsgtxt)
+        self.request.send(hmsg)
+    #
+    # end redis usage
+    #
+
+    def handle(self):
+        starttime = datetime.datetime.now()
+        try:
+            syslog.syslog(syslog.LOG_DEBUG, 'handler invoked')
+            hlen = 0
+            hlenRec = ''
+            while True:
+                b = self.request.recv(1)
+                if b == '':
+                    syslog.syslog(syslog.LOG_WARNING, 'recv returned unexpectedly, rejecting.')
+                    return
+                if b == '\n':
+                    hlen = int(hlenRec)
+                    break
+                hlenRec += b
+                if len(hlenRec) > 10:
+                    syslog.syslog(syslog.LOG_WARNING, 'rejecting bad header: %s' % (hlenRec))
+                    self.request.send('invalid header: %s\n' % (hlenRec))
+                    return
+            
+            syslog.syslog(syslog.LOG_DEBUG, 'processing')
+            syslog.syslog(syslog.LOG_DEBUG, 'msg header len: %i' % (hlen))
+            
+            if hlen > MAX_MSG_SIZE or hlen == 0:
+                syslog.syslog(syslog.LOG_WARNING, 'rejecting bad msg size header: %s' % (hlen))
+                self.request.send('invalid msg size: %i\n' % (hlen))
+                return
+
+            BUFSZ = 1024
+            data = ''
+            while len(data) < hlen:
+                sz = BUFSZ
+                rem = hlen - len(data)
+                if rem < BUFSZ:
+                    sz = rem
+                b = self.request.recv(sz)
+                syslog.syslog(syslog.LOG_DEBUG, 'read %i bytes' % (len(b)))
+                data += b
+
+            dataobj = ''
+            syslog.syslog(syslog.LOG_DEBUG, 'total bytes read: %i' % (len(data)))
+                        
+            try:
+                dataobj = json.loads(data)
+            except ValueError, e:
+                self.handleError(str(e))
+                return
+
+            token = dataobj["user"]["token"]
+
+            syslog.syslog(syslog.LOG_INFO, 'pollenc request handler invoked for token %s' % (token))
+            cur_thread = threading.currentThread()
+            responseQueue = 'POLLENC_REPLYTO_QUEUE_%s_%s' % (cur_thread.getName(), dataobj["user"]["token"])
+            dataobj["reply"] = responseQueue
+            qname = self.getQName(dataobj['compiler']);
+            self.write(qname, json.dumps(dataobj))
+
+            while True:
+                response = self.read(responseQueue)
+                hmsg = "%i\n%s" % (len(response), response)
+                self.request.send(hmsg)
+                dataobj = json.loads(response)
+                if dataobj['type'] != 'response':
+                    continue
+
+                break
+            self.sendStats(starttime)     
+        except Exception, e:
+            self.handleError(str(e))
+        except:
+            e = sys.exc_info()[0]
+            self.logexception(1)
+            etxt = 'pollenc exception %s (%s)' % (e.__class__, e)
+            self.handleError(etxt)
+
+        return
+
+    def sendErrorStats(self):
+        state = 'warning'
+
+        #rmmonitor.send({'host': config.riemann['clienthost'], 'service': 'pollenc exception', 'metric': 1, 'description': 'pollenc txn failed', 'state': state})
+        return
+
+    def sendStats(self, starttime):
+        stoptime = datetime.datetime.now()
+        dur = stoptime - starttime
+        mdur = dur.microseconds
+        if mdur > 0:
+            mdur = mdur / 1000 # we only care about milliseconds
+        else:
+            raise Exception('PollenServer: impossible duration %s' % (mdur))
+        state = 'ok'
+        if mdur > 1000:
+            state = 'warning'
+        elif mdur > 10000:
+            state = 'critical'
+       
+        #rmmonitor.send({'host': config.riemann['clienthost'], 'service': 'pollenc txn-dur', 'metric': mdur, 'description': 'pollenc txn duration in milliseconds', 'state': state})
+        return
+
+    def logexception(self, includetraceback = 0):
+        exctype, exception, exctraceback = sys.exc_info()
+        excclass = str(exception.__class__)
+        message = str(exception)
+
+        if not includetraceback:
+            syslog.syslog(syslog.LOG_ERR, "%s: %s" % (excclass, message))
+        else:
+            excfd = StringIO.StringIO()
+            traceback.print_exception(exctype, exception, exctraceback, None,
+                                      excfd)
+            for line in excfd.getvalue().split("\n"):
+                syslog.syslog(syslog.LOG_ERR, line)
+
+
+class PollencServer(SocketServer.ThreadingMixIn, SocketServer.TCPServer):
+  pass
+
+
+if __name__ == '__main__':
+
+    syslog.syslog(syslog.LOG_INFO, 'service starting using redis host %s:%s' % ((REDIS_HOST, REDIS_PORT))) 
+    
+    address = ((TCP_HOST, TCP_PORT)) 
+    server = PollencServer(address, PollencRequestHandler)
+
+    server.serve_forever()
+
+    syslog.syslog(syslog.LOG_INFO, 'service stopping')
+
+    server.socket.close()
+
---
+++ pollenc_sock.py
@@ -0,0 +1,409 @@
+#!/usr/bin/env python
+
+# Copyright 2014 Amaret Inc. All rights reserved.
+
+import sys
+import subprocess
+import shutil
+import shlex
+import argparse #cmdline overrides configrgs
+import json
+import zipfile
+import os
+import base64
+import random
+import time
+import hashlib
+import socket
+import redis
+
+
+def rmfile(f):
+    try:
+            os.remove(f)
+    except:
+            pass
+
+def rmdir(d):
+    try:
+            shutil.rmtree(d)
+    except:
+            pass
+
+
+class Pollenc:
+
+  def __init__ (self, args):
+    self.bundleNames = []
+    self.env = args.env
+    self.prn = args.prn
+    self.trace = args.trace # superset of verbose output
+    self.verbose = True if args.verbose and not args.trace else False
+    self.args = args
+    self.aid = str(os.getpid()) + '_' + str(random.randint(1, 10000))
+    self.workname = 'pollenc_' + self.aid
+    self.workzip = '/tmp/' + self.workname + '_src.zip' 
+    self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+    self.sock.connect((args.host, args.port))
+
+    #
+    # begin comm
+    #
+  def write(self, msg):
+        hmsg = "%i\n%s" % (len(msg), msg)
+        self.sock.send(hmsg)
+
+  def read(self):
+
+        hlen = 0
+        hlenRec = ''
+        while True:
+            b = self.sock.recv(1)
+            if b == '\n':
+                hlen = int(hlenRec)
+                break
+            hlenRec += b
+
+        BUFSZ = 1024
+        r = ''
+        while len(r) < hlen:
+            sz = BUFSZ
+            rem = hlen - len(r)
+            if rem < BUFSZ:
+                sz = rem
+            b = self.sock.recv(sz)
+            r += b
+        return r
+    #
+    # end comm
+    #
+
+  def makezip(self):
+       rmfile(self.workzip)
+       self.makeBundleZip()
+
+  def zipBundles(self, zip, paths):
+        tmpdir = '/tmp/' + self.workname
+        self.dbglog("Preparing work dirs...", "client tmp directory: %s" % tmpdir )
+               
+        for src in paths:
+            if src.find('*')!=-1 or src.find('?')!=-1:
+                wildcardLst = glob.glob(src)
+                if len(wildcardLst) > 0:
+                    self.zipBundles(zip, wildcardLst) #recurse for wildcard
+                    continue
+            if not os.path.exists(src):
+                self.bundleNames.append(src)
+                continue #system bundle
+            rmdir(tmpdir)
+            bundleName = self.getBundleName(src)
+            if bundleName in self.bundleNames:
+                continue #dupe -i
+            self.bundleNames.append(bundleName)
+            shutil.copytree(src, tmpdir + '/' + bundleName)
+            self.zipdir(tmpdir, zip)
+            rmdir(tmpdir)
+
+  def makeBundleZip(self):
+        zip = zipfile.ZipFile(self.workzip, 'w')
+        self.dbglog("Preparing bundles...", "client bundles: %s" % self.args.bundles)
+        file_count = 1
+        if self.args.bundles != None:
+            for src in self.args.bundles:
+              path, dirs, files = os.walk(src).next()
+              file_count += len(files)
+            msg = "Preparing %s files..." if file_count > 1 else "Preparing %s file..." 
+            self.dbglog(msg % str(file_count))
+            self.zipBundles(zip, self.args.bundles)
+            zip.close()
+
+  def unzip(self,src): 
+      tmpzip = 'a.zip' 
+      try: 
+          binfile = open(tmpzip, 'wb')
+          binfile.write(src)
+          binfile.close()
+          z = zipfile.ZipFile(tmpzip)
+          z.extractall('.')
+          rmfile(tmpzip)
+      except Exception, e:
+          print("argh! %s" % (e))
+
+
+  def filenameOk(self, file):
+        if file.endswith('.zip'):
+            return False
+        if file.endswith('stderr'):
+            return False
+        if file.endswith('stdout'):
+            return False
+        if file.endswith('.out'):
+            return False
+        if file.endswith('.hex'):
+            return False
+        return True
+
+  def zipdir(self, path, zip):
+        origpath = os.getcwd()
+        os.chdir(path)
+        namelist = ''
+        for root, dirs, files in os.walk('.'):
+            for file in files:
+                if self.filenameOk(file):
+                    namelist += root + '/'+ file + ' '
+                    zip.write(os.path.join(root, file))
+        os.chdir(origpath)
+
+  def printStdErr(self):
+        for root, dirs, files in os.walk(self.args.outdir):
+            for f in files:
+                if f.endswith('err'):
+                    print 'msgs found in stderr from server:'
+                    with open(root + '/' + f, 'r') as fin:
+                        print fin.read()
+
+
+  def getBundleName(self, path):
+        abspath = os.path.abspath(path)
+        (r, m) = os.path.split(abspath)
+        return m
+
+  def getData(self):
+	file = open(self.workzip, "rb")
+        data = ''
+        while True:
+            chunk = file.read(1024)
+            if not chunk:
+                break  # EOF
+            data += chunk
+        file.close()
+        return data
+
+  def getRelToTmpDirName(self):
+        abspath = os.path.abspath(self.args.entry)
+        (r, m) = os.path.split(abspath)
+        l = abspath.split("/")
+        llen = len(l)
+        if llen < 3:
+            raise Exception('filename must be in <bundle>/<package>')
+        r = l[llen -3] + '/' + l[llen -2] + '/' + l[llen -1]
+        return r
+
+  # verbose output shows the phases for users.
+  # trace output has internal info for debugging.
+  # if trace is true verbose is set to false
+  def dbglog(self, phase, msg=None):
+      if not self.verbose and not self.trace:
+        return
+      if self.verbose:
+        print phase
+        return
+      if self.trace:
+        print "DBGLOG: " + phase
+        if not msg:
+            return
+        didprint = False
+        if 'aid' in msg:
+          print "  dbg 'aid' " + str(msg['aid'])
+          didprint = True
+        if 'reply' in msg:
+          print "  dbg 'reply' " + str(msg['reply'])
+          didprint = True
+        if 'tid' in msg:
+          print "  dbg 'tid' " + str(msg['tid'])
+          didprint = True
+        if 'type' in msg:
+          print "  dbg 'type' " + str(msg['type'])
+          didprint = True
+        if not didprint:
+          print "   " + str(msg)
+        
+
+  def sendCompileRequest(self):    
+    tid = hashlib.sha1(str(time.time()) + '-' + self.args.token).hexdigest()    
+    b64data = base64.b64encode(self.getData())
+
+    self.reply = 'POLLENC_REPLYTO_QUEUE_%s' % self.aid
+
+    jsonobj = {
+      'compiler': self.args.toolchain,
+      'tid': tid,
+      'aid': self.aid,
+      'reply': self.reply,
+      'type': 'request', 
+      'service': 'compile', 
+      'bundles': self.bundleNames,
+      'env': self.env,
+      'prn': self.prn,
+      'trace': self.trace,
+      'user': {
+        'token': self.args.token, 
+        'id': 0, 
+        'name': 'None'
+      },
+      'content' :  {
+        'source':  b64data, 
+        'entry': self.getRelToTmpDirName(),
+        'mcu': self.args.mcu
+      }
+    }
+
+    self.dbglog("Sending compile request" +  "...", jsonobj)
+    jsonstr = json.dumps(jsonobj)
+    #print 'sending json:\n' + jsonstr + '\n'
+    self.write(jsonstr)
+    
+
+
+  def run(self):
+    self.makezip()
+    self.dbglog("Pollenc running...")
+    self.sendCompileRequest()
+
+    while True:
+
+      self.dbglog("Wait for response...", self.reply)
+      r = self.read()
+      workobj   = json.loads(r)
+      self.dbglog("Got response...", workobj)
+      if workobj['type'] == 'userlog':
+          print ('[server message] %s' % (workobj['content']['source'])) 
+          continue
+      if workobj['type'] != 'response': 
+          continue
+      if workobj['content']['error'] != 'None': 
+          print ('pollenc error! %s' % (workobj['content']['error'])) 
+          return
+      break
+
+    rmfile(self.workzip)
+
+    self.dbglog("Got workobj...", workobj)
+    b64 = workobj['content']['source']
+    zipbytes = base64.b64decode(b64)
+    origpath = os.getcwd()
+    os.chdir(self.args.outdir)
+    self.unzip(zipbytes)
+    os.chdir(origpath)
+    self.dbglog("Pollenc done. Build output in " + self.args.outdir)
+    self.printStdErr() #look for stderr and play msgs
+
+
+
+if __name__ == "__main__":
+  
+  parser = argparse.ArgumentParser()
+
+  # The host and port arguments are for later. Right now pollenc is using redis not sockets. 
+  parser.add_argument('--host', dest='host', action='store', help='wind.io host', default="127.0.0.1")
+  #parser.add_argument('--host', dest='host', action='store', help='wind.io host', default="passage.wind.io")
+  parser.add_argument('--port', dest='port', action='store', help='wind.io port', default=2323, type=int)
+  # 26164 is open by a network utility port scan, not many open ports!
+  #parser.add_argument('--port', dest='port', action='store', help='wind.io port', default=26164, type=int)
+
+  # The token argument is for later. It will be a user specified token that identifies them. 
+  parser.add_argument('--token', dest='token', action='store', help='user credential', required=False)
+
+  ###
+  # Here we need to collect the rest of the arguments that a user may provide on the commandline for the 
+  # translator and compiler. For example, the input bundles included, the output directory, any cflags, 
+  # other flags, etc.
+  ####
+  group = parser.add_mutually_exclusive_group()
+
+  parser.add_argument('-b', '--bundle', dest='bundles',  action='append', \
+          help="pollen bundle. Paths prefixed with '@' are on server, the rest will be uploaded.", required=False)
+
+  parser.add_argument('-i', '--include', dest='includes', action='append', \
+          help='c file to be uploaded to server.', required=False)
+
+  group.add_argument('-t', '--toolchain', dest='toolchain', action='store', \
+          help='toolchain (compiler).', required=False, \
+          choices = ['avr-gcc', "msp430-gcc", "arm-gcc", "localhost-gcc"])
+
+  parser.add_argument('-m', '--mcu', dest='mcu', action='store', help='microcontroller', required=False)
+
+  parser.add_argument('--cflags', dest='cflags', action='store', \
+          help='extra options to pass to C compiler.', required=False)
+
+
+  helpStr = ('pollen module used for pollen.environment. ' +
+            'Path prefixed with "@" is on server, else will be uploaded.')
+  parser.add_argument('-e', '--environment', dest='env', action='store', help=helpStr, required=False)
+
+  helpStr = 'pollen module that will implement the print protocol. ' \
+          + 'Path prefixed with "@" is on server, else will be uploaded.'
+  parser.add_argument('-p', '--print-module', dest='prn', action='store', help=helpStr, required=False) 
+
+  parser.add_argument('-o', '--out',   dest='outdir', action='store', help='output dir', default='./build')
+
+  group.add_argument('--props', dest='props', action='store', \
+          help='properties file (for toolchain compiler and options).', required=False)
+
+  parser.add_argument('--trace', dest='trace', action='store_true', help=argparse.SUPPRESS) 
+
+  parser.add_argument('-v', '--verbose', dest='verbose', action='store_true', help='verbose output')
+
+  parser.add_argument('entry', nargs='?', action='store', \
+          help='top level pollen file (entry point). Qualify with bundle and package.')
+
+  args = parser.parse_args()
+
+  if args.host == None:
+    args.host = ""
+
+  if args.token == None:
+    args.token = 'TOKEN-' + str(random.randint(1, 10000))
+
+  args.translateOnly = False
+  if (args.props == None and args.toolchain == None):
+	  args.translateOnly = True
+
+  args.build = not args.translateOnly
+
+  if not args.translateOnly and args.mcu == None:
+      print("If toolchain is specified then -mcu <microcontroller> must also be specified") 
+      sys.exit(0)
+
+  if args.translateOnly and args.mcu != None:
+      print("If -mcu <microcontroller> is specified then toolchain must also be specified")
+      sys.exit(0)
+
+  if os.path.exists(args.outdir):
+      rmdir(args.outdir)
+  os.mkdir(args.outdir)
+
+  args.entry = os.path.abspath(args.entry)
+  (p, m) = os.path.split(args.entry)
+  (bname, p) = os.path.split(p)
+  if args.bundles == None:
+     args.bundles = []
+  if bname not in args.bundles:
+     args.bundles.append(bname)
+
+  # if the environment module is local put its bundle in bundle list
+  if args.env != None: 
+      if args.env[0] != '@': # not on the server
+          args.env = os.path.abspath(args.env) 
+          (p1, m) = os.path.split(args.env) 
+          (bname, p2) = os.path.split(p1) 
+          args.env = p2 + "/" + m
+          if bname not in args.bundles: 
+              args.bundles.append(bname)
+
+  # if the print module impl is local put its bundle in bundle list
+  if args.prn != None: 
+      if args.prn[0] != '@': # not on the server
+          args.prn = os.path.abspath(args.prn) 
+          (p1, m) = os.path.split(args.prn) 
+          (bname, p2) = os.path.split(p1) 
+          args.prn = p2 + "/" + m
+          if bname not in args.bundles: 
+              args.bundles.append(bname)
+
+  if not os.path.exists(args.entry):
+     print "Module " + args.entry + " not found"
+     sys.exit()
+
+  Pollenc(args).run() 
+
