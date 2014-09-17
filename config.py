e250ae60b4125588529572979d28f9b889f61d95
Amichi Amar <ami@amaret.com>
2014-09-17 15:04:08 -0700

    modified script to handle cleaner output.

--- config.py
+++ config.py
@@ -2,23 +2,23 @@
 Global configuration file
 '''
 mongo = {
-  'host': 'localhost', # '10.244.138.56'
+  'host': 'redb.amaret.com',
   'port': 27017,
 }
 
 pollenc_tcp = {
-    'interface': 'pcc.amaret.com', 
-    'port': 5140,
+  'interface': 'pcc.amaret.com', 
+  'port': 5140,
 }
 
 redisQueues = {
-        'arm-none-eabi-gcc': 'POLLEN_CLC_ARM_NONE_EABI_GCC_1_0',
-        'avr-gcc': 'POLLEN_CLC_AVR_GCC_1_0',
-        'efm32-gcc': 'POLLEN_CLC_ARM_NONE_EABI_GCC_1_0',
-        'localhost-gcc': 'POLLEN_CLC_LOCALHOST_1_0'
+  'arm-none-eabi-gcc': 'POLLEN_CLC_ARM_NONE_EABI_GCC_1_0',
+  'avr-gcc': 'POLLEN_CLC_AVR_GCC_1_0',
+  'efm32-gcc': 'POLLEN_CLC_ARM_NONE_EABI_GCC_1_0',
+  'localhost-gcc': 'POLLEN_CLC_LOCALHOST_1_0'
 }
 
 clcConstants = {
-        'MAX_MSG_SIZE' : 1000000,
+  'MAX_MSG_SIZE' : 1000000,
 }
 
